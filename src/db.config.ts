475b7b594e24be3647e0ce1f7fb4190de612c94e
Rajesh Songara <49251048+rajeshagryd@users.noreply.github.com>
2024-03-02 21:11:10 +0530

    Update db.config.ts

--- src/db.config.ts
+++ src/db.config.ts
@@ -2,11 +2,11 @@ import { Pool } from "pg"
 console.log((process.env));
 
 const connectDB = new Pool({
-    user: process.env.USERNAME',
-    host: process.env.HOST_NAME ',
-    database: process.env.DATABASE_NAME',
-    password: String(process.env.DATABASE_PASSWORD)',
-    port: Number(process.env.DATABASE_PORT),
+    user: process.env.USERNAME || 'postgres',
+    host: process.env.HOST_NAME || '65.2.71.93',
+    database: process.env.DATABASE_NAME || 'postgres',
+    password: String(process.env.DATABASE_PASSWORD) || 'postgres',
+    port: Number(process.env.DATABASE_PORT) || 5432,
     
 })
 
