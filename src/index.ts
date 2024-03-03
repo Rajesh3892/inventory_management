b158a0ce33fb4c38a113460ab6b778bf6c8f22ff
Rajesh Songara <49251048+rajeshagryd@users.noreply.github.com>
2024-03-03 15:59:14 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import http from "http"
 import GroceryController from "./user/controllers/grocery.controllers";
 import InventoryController from "./admin/controllers/inventory.controllers";
 import UserController from "./user/controllers/user.controllers";
-// import connectDB from "./db.config";
+import connectDB from "./db.config";
 
 const app: Application = express();
 
@@ -20,36 +20,36 @@ const server = http.createServer(app)
 
 
 
-app.post('/', async (req: Request, res: Response) => {
-  try {
-    res.json({message: "This is home page json"});
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+// app.post('/', async (req: Request, res: Response) => {
+//   try {
+//     res.json({message: "This is home page json"});
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Internal Server Error' });
+//   }
+// });
 
 
 // User routes
-// app.get("/", GroceryController.getAllItems)
-// app.post("/create_user", UserController.createUser)
-// app.post("/orders", GroceryController.myOrders)
+app.get("/", GroceryController.getAllItems)
+app.post("/create_user", UserController.createUser)
+app.post("/orders", GroceryController.myOrders)
 
-// // Admin routes
-// app.post('/admin', InventoryController.addItem);
-// app.get('/admin', InventoryController.allItems);
-// app.delete('/admin/:item_id', InventoryController.removeItem);
-// app.put('/admin/:item_id', InventoryController.updateItem);
+// Admin routes
+app.post('/admin', InventoryController.addItem);
+app.get('/admin', InventoryController.allItems);
+app.delete('/admin/:item_id', InventoryController.removeItem);
+app.put('/admin/:item_id', InventoryController.updateItem);
 
 
 
-server.listen(port, ()=>{
-    console.log(`Server listen on port ${port}`);
-})
-// connectDB.connect().then(() => {
-//     server.listen(port, ()=>{
-//         console.log(`Server listen on port ${port}`);
-//     })
-// }).catch((err)=>{
-//     console.error(err);
+// server.listen(port, ()=>{
+//     console.log(`Server listen on port ${port}`);
 // })
+connectDB.connect().then(() => {
+    server.listen(port, ()=>{
+        console.log(`Server listen on port ${port}`);
+    })
+}).catch((err)=>{
+    console.error(err);
+})
